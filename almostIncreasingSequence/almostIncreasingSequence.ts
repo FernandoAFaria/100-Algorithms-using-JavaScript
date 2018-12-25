699188ba8b7e7f5943e216a16a17b5c6b532ff46
Fernando Faria <fernandoafaria@yahoo.com>
2018-12-25 17:20:13 -0500

    almostincreasing

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -3,7 +3,7 @@ function almostIncreasingSequence(sequence: number[]): boolean {
     let count = 0;
 
     for(let i = 0; i < sequence.length; i++){
-        count = sequence[i] > sequence[i+1] ? count+1 : count;
+        count = sequence[i] >= sequence[i+1] ? count+1 : count;
         if(sequence[i] <= sequence[i-2] && sequence[i+1] <= sequence[i - 1]) { 
             return false;
         }
@@ -17,3 +17,5 @@ function almostIncreasingSequence(sequence: number[]): boolean {
 
 console.log(almostIncreasingSequence([1, 3, 2, 1])) 
 console.log(almostIncreasingSequence([1, 3, 2])) 
+
+console.log(almostIncreasingSequence([0, 1, 2, 3, 3, 5, 6 ,7, 2, 8])); 
